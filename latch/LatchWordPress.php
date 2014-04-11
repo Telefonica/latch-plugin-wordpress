e9e56a902096826b638eafea6d60ef0c0a5fbe6b
Javier Espinosa <javier.espinosa@11paths.com>
2014-04-11 11:52:10 +0100

    Rename file + Update repo for Worpress.org

--- latch/LatchWordPress.php
+++ latch/LatchWordPress.php
@@ -5,7 +5,7 @@
 * Description: Latch WordPress integration
 * Author: Eleven Paths
 * Author URI: http://www.elevenpaths.com/
-* Version: 1.0.3
+* Version: 1.1
 * Compatibility: WordPress 3.5
 * Text Domain: latch
 */
@@ -33,22 +33,22 @@ require_once("Latch.php");
 require_once("LatchResponse.php");
 require_once("Error.php");
 
-class LatchWP {
-	
+class LatchWordPress {
+
 	/* -------- GLOBAL SETTINGS (admin) --------- */
-	
+
 	function action_admin_init() {
-		add_settings_section('latch_settings', 'Global settings', array('LatchWP', 'latch_settings_content'), 'latch_settings');
-		add_settings_field('latch_appId', 'Application ID', array('LatchWP', 'latch_settings_appId'), 'latch_settings', 'latch_settings');
-		add_settings_field('latch_appSecret', 'Secret key', array('LatchWP', 'latch_settings_appSecret'), 'latch_settings', 'latch_settings');
-        add_settings_field('latch_host', 'API URL', array('LatchWP', 'latch_settings_host'), 'latch_settings', 'latch_settings');
-		register_setting('latch_settings', 'latch_appId', array('LatchWP', 'latch_validate_appId'));
-		register_setting('latch_settings', 'latch_appSecret', array('LatchWP', 'latch_validate_appSecret'));
-        register_setting('latch_settings', 'latch_host', array('LatchWP', 'latch_validate_host'));
+		add_settings_section('latch_settings', 'Global settings', array('LatchWordPress', 'latch_settings_content'), 'latch_settings');
+		add_settings_field('latch_appId', 'Application ID', array('LatchWordPress', 'latch_settings_appId'), 'latch_settings', 'latch_settings');
+		add_settings_field('latch_appSecret', 'Secret key', array('LatchWordPress', 'latch_settings_appSecret'), 'latch_settings', 'latch_settings');
+        add_settings_field('latch_host', 'API URL', array('LatchWordPress', 'latch_settings_host'), 'latch_settings', 'latch_settings');
+		register_setting('latch_settings', 'latch_appId', array('LatchWordPress', 'latch_validate_appId'));
+		register_setting('latch_settings', 'latch_appSecret', array('LatchWordPress', 'latch_validate_appSecret'));
+        register_setting('latch_settings', 'latch_host', array('LatchWordPress', 'latch_validate_host'));
 	}
 
 	function action_admin_menu() {
-		add_options_page('Latch settings', 'Latch settings', 'manage_options', 'latch_wordpress', array('LatchWP', 'latch_settings_page'));
+		add_options_page('Latch settings', 'Latch settings', 'manage_options', 'latch_wordpress', array('LatchWordPress', 'latch_settings_page'));
 	}
 
 	function latch_settings_content() {
@@ -64,12 +64,12 @@ class LatchWP {
 		$appSecret = esc_attr(get_option('latch_appSecret'));
 		echo '<input id="latch_appSecret" name="latch_appSecret" size="90" maxlength="40" type="text" value="' . $appSecret . '" />';
 	}
-	
+
     function latch_settings_host() {
         $host = esc_attr(get_option('latch_host'));
         echo '<input id="latch_host" name="latch_host" size="90" type="text" value="' . $host . '" />';
     }
-	
+
 	function latch_settings_page() {
 		echo '<div><h2>Latch settings</h2>';
 		echo '<form action="options.php" method="post">';
@@ -78,7 +78,7 @@ class LatchWP {
 		echo '<br /><input name="Submit" type="submit" value="' . __('Save Changes') . '" />';
 		echo '</form></div>';
 	}
-	
+
 	function latch_validate_appId($appId){
 		if (!empty($appId) && strlen($appId) != 20) {
 			add_settings_error('latch_invalid_appId', 'latch_invalid_appId', __('Invalid application ID'));
@@ -87,7 +87,7 @@ class LatchWP {
 			return $appId;
 		}
 	}
-	
+
 	function latch_validate_appSecret($appSecret){
 		if (!empty($appSecret) && strlen($appSecret) != 40) {
 			add_settings_error('latch_invalid_appSecret', 'latch_invalid_appSecret', __('Invalid secret key'));
@@ -100,18 +100,18 @@ class LatchWP {
     function latch_validate_host($host) {
         return rtrim($host, '/');
     }
-	
-	
+
+
 	/* -------- PROFILE SETTINGS (current user) --------- */
-	
+
 	function action_profile_personal_options() {
 		global $user_id, $is_profile_page;
-		
+
 		$latch_id = trim( get_user_option('latch_id', $user_id ) );
-		
+
 		echo "<h3>" . __( 'Latch Setup', 'latch' ) . "</h3>\n";
 		echo '<table class="form-table"><tbody><th>';
-		
+
 		if(empty($latch_id)) {
 			echo '<label for="latch_token">' . __('Latch token','latch') . '</label></th><td>';
 			echo '<input name="latch_token" id="latch_token"  maxlength="10" type="text" size="25" />';
@@ -121,10 +121,10 @@ class LatchWP {
 		}
 		echo '</td></tr></tbody></table>';
 	}
-	
+
 	function action_user_profile_update_errors($errors) {
 		global $user_id;
-		
+
 		$appId = get_option('latch_appId');
 		$appSecret = get_option('latch_appSecret');
 		$token =  $_POST['latch_token'];
@@ -132,28 +132,28 @@ class LatchWP {
         if (!empty($host)) {
             Latch::setHost($host);
         }
-		
+
 		if (!empty($appId) && !empty($appSecret) ) {
 			$api = new Latch($appId, $appSecret);
 			$userLatchId = get_user_option('latch_id', $user_id);
-			
-			if (!empty($token) && empty($userLatchId)) { 
+
+			if (!empty($token) && empty($userLatchId)) {
 				$pairResponse = $api->pair($token);
 				$responseData = $pairResponse->getData();
-			
+
 				if (!empty($responseData)) {
 					$accountId = $responseData->{"accountId"};
 				}
 
 				if (!empty($accountId)) {
 					update_user_option($user_id, 'latch_id', $accountId, true);
-				} elseif ($pairResponse->getError() == NULL) { 
+				} elseif ($pairResponse->getError() == NULL) {
                     // If Account ID is empty and no error fields are found, there are problems with the connection to the server
 				    $errors->add('latch_pairing_error', 'Latch pairing error: Cannot connect to the server. Please, try again later.');
 				} else {
 					$errors->add('latch_pairing_error', 'Latch pairing error: ' . __($pairResponse->getError()->getMessage()) );
 				}
-				
+
 			} else if ($_POST['latch_unpair']) {
 				$api->unpair(get_user_option('latch_id', $user_id));
                                 update_user_option($user_id, 'latch_id', null, true);
@@ -163,8 +163,8 @@ class LatchWP {
 
 
 	/* -------- AUTHENTICATION --------- */
-	
-	function filter_authenticate($user, $username = '', $password = '') {			
+
+	function filter_authenticate($user, $username = '', $password = '') {
 		if (!is_a($user, 'WP_User')) {
 			return $user;
 		} else {
@@ -174,60 +174,60 @@ class LatchWP {
             if (!empty($host)) {
                 Latch::setHost($host);
             }
-	
+
 			if (!empty($appId) && !empty($appSecret) ) {
 				remove_action('authenticate', 'wp_authenticate_username_password', 20);
 				$user = wp_authenticate_username_password(null, $username, $password);
-	
+
 				if (isset($_POST["latch_two_factor"])) {
 					$expectedToken = get_user_option('latch_two_factor', $user->ID);
 					update_user_option($user->ID, 'latch_two_factor', null, true);
-										
+
 					if (!empty($expectedToken) && $_POST["latch_two_factor"] === $expectedToken) {
 						return $user;
 					} else {
 						return new WP_Error('latch_invalid_token', __('<strong>ERROR</strong>: Invalid token', 'latch'));
 					}
 				}
-				
+
 				$latch_accountId = get_user_option('latch_id', $user->ID);
-				
+
 				if (!empty($latch_accountId)) {
 				    $api = new Latch($appId, $appSecret);
 					$statusResponse = $api->status($latch_accountId);
-	
+
 					$responseData = $statusResponse->getData();
 					$responseError = $statusResponse->getError();
-					
+
 					//error_log(print_r($responseData, true));
 					//error_log(print_r($responseError, true));
-									
+
 					// If something goes wrong, disable Latch temporary or permanently to prevent blocking the user
 					if (empty($statusResponse) || (empty($responseData) && empty($responseError))) {
-						return $user;						
+						return $user;
 					} else {
 						if (!empty($responseError)) {
 							if ($responseError->getCode() == 201) {
-								// If the account is externally unpaired, apply the changes in WP database 
+								// If the account is externally unpaired, apply the changes in WP database
 								update_user_option($user->ID, 'latch_id', null, true);
 								update_user_option($user->ID, 'latch_two_factor', null, true);
-							}					
+							}
 							return $user;
 						}
 					}
-						
+
 					if (!empty($responseData) && $responseData->{"operations"}->{$appId}->{"status"} === "on") {
 						$two_factor_token = $responseData->{"operations"}->{$appId}->{"two_factor"}->{"token"};
-						
+
 						if (!empty($two_factor_token)) {
 							update_user_option($user->ID, 'latch_two_factor', $two_factor_token, true);
-							
+
 							include('two_factor.php');
 							die;
 						}
-						
+
 						update_user_option($user->ID, 'latch_two_factor', null, true);
-						
+
 						return $user;
 					} else {
 						//return new WP_Error('latch_account_blocked', __('<strong>ERROR</strong>: The account is blocked by Latch', 'latch'));
@@ -243,10 +243,10 @@ class LatchWP {
 	}
 }
 
-add_action('admin_init', array('LatchWP', 'action_admin_init'));
-add_action('admin_menu', array('LatchWP', 'action_admin_menu'));
-add_action('profile_personal_options', array('LatchWP', 'action_profile_personal_options'));
-add_action('user_profile_update_errors', array('LatchWP', 'action_user_profile_update_errors'), 20, 1);
-add_filter('authenticate', array('LatchWP', 'filter_authenticate'), 50, 3);
+add_action('admin_init', array('LatchWordPress', 'action_admin_init'));
+add_action('admin_menu', array('LatchWordPress', 'action_admin_menu'));
+add_action('profile_personal_options', array('LatchWordPress', 'action_profile_personal_options'));
+add_action('user_profile_update_errors', array('LatchWordPress', 'action_user_profile_update_errors'), 20, 1);
+add_filter('authenticate', array('LatchWordPress', 'filter_authenticate'), 50, 3);
 
 ?>
---
+++ latch/readme.txt
@@ -0,0 +1,38 @@
+=== LatchWordpress ===
+Contributors: it.elevenpaths
+Tags: authentication, security, plugin
+Requires at least: 1.5
+Tested up to: 3.8.2
+Stable tag: 1.1
+License: LGPLv2.1
+
+
+This Plugin allows developers to integrate Latch on his/her WordPress service.
+
+== Description ==
+
+This Plugin allows developers to integrate Latch on his/her WordPress service. Latch is a service that lets end-users add an extra level of security to their online accounts and services. 
+With this version of the Plugin, developers can pair and unpair users and check their latches status. For more information please visit https://latch.elevenpaths.com or the Plugin page on GitHub https://github.com/ElevenPaths/latch-plugin-wordpress
+
+
+
+== Installation ==
+
+* When the account is activated, the user will be able to create applications with Latch and access to developer documentation, including existing SDKs and plugins. The user has to access again to [Developer area](https://latch.elevenpaths.com/www/developerArea), and browse his applications from **"My applications"** section in the side menu.
+
+* When creating an application, two fundamental fields are shown: **"Application ID"** and **"Secret"**, keep these for later use. There are some additional parameters to be chosen, as the application icon (that will be shown in Latch) and whether the application will support OTP  (One Time Password) or not.
+
+* From the side menu in developers area, the user can access the **"Documentation & SDKs"** section. Inside it, there is a **"SDKs and Plugins"** menu. Links to different SDKs in different programming languages and plugins developed so far, are shown.
+
+* Once the administrator has downloaded the plugin, it has to be added as a plugin in its administration panel in WordPress. Click on **"Plugins"** and **"Add new"**. It will show a form where you can browse and select previously downloaded ZIP file.
+
+* Go to **"Latch settings"**, inside **"Settings"** and introduce **"Application ID"** and **"Secret"** previously generated. The administrator can now save the changes clicking on **"Save"**. If everything is ok, a confirmation message will be received.
+
+* From now on, on user's profile, a new textbox will appear, inside **"Profile"** menu, where the token generated from the app should be introduced.
+
+
+== Changelog ==
+
+= 1.1 =
+*Initial release
+
