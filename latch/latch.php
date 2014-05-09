4e3d58d07e4b7614c9b526316cb554abc7a89587
Javier Espinosa <javier.espinosa@11paths.com>
2014-05-09 12:56:43 +0100

    Class rename

--- latch/latch.php
+++ latch/latch.php
@@ -29,7 +29,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-require_once("sdk/Latch.php");
+require_once("sdk/LatchSDK.php");
 require_once("sdk/LatchResponse.php");
 require_once("sdk/Error.php");
 
--- latch/sdk/LatchSDK.php
+++ latch/sdk/LatchSDK.php
@@ -20,16 +20,16 @@
   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-final class Latch {
+final class LatchSDK {
 	private static $API_VERSION = "0.6";
 	public static $API_HOST = "https://latch.elevenpaths.com";
 	public static $API_CHECK_STATUS_URL = "/api/0.6/status";
-	public static $API_PAIR_URL = "/api/0.6/pair"; 
+	public static $API_PAIR_URL = "/api/0.6/pair";
 	public static $API_PAIR_WITH_ID_URL = "/api/0.6/pairWithId";
 	public static $API_UNPAIR_URL =  "/api/0.6/unpair";
 
         public static $PROXY_HOST = NULL;
-        
+
         public static $CA_CERTIFICATE_PATH = NULL;
 
 	public static $AUTHORIZATION_HEADER_NAME = "Authorization";
@@ -37,26 +37,26 @@ final class Latch {
 	public static $AUTHORIZATION_METHOD = "11PATHS";
 	public static $AUTHORIZATION_HEADER_FIELD_SEPARATOR = " ";
 
-	public static $UTC_STRING_FORMAT = "Y-m-d H:i:s"; 
+	public static $UTC_STRING_FORMAT = "Y-m-d H:i:s";
 
 	private static $HMAC_ALGORITHM = "sha1";
-	
+
 	public static $X_11PATHS_HEADER_PREFIX = "X-11Paths-";
 	private static $X_11PATHS_HEADER_SEPARATOR = ":";
 
-	
+
 	public static function setHost($host) {
             Latch::$API_HOST = $host;
 	}
-	
+
         public static function setProxy($host) {
             self::$PROXY_HOST = $host;
 	}
-	
+
         public static function setCACertificatePath($certificatePath) {
             self::$CA_CERTIFICATE_PATH = $certificatePath;
         }
-        
+
 	/**
 	 * The custom header consists of three parts, the method, the appId and the signature.
 	 * This method returns the specified part if it exists.
@@ -73,7 +73,7 @@ final class Latch {
 		}
 		return "";
 	}
-	
+
 	/**
 	 *
 	 * @param $authorizationHeader Authorization HTTP Header
@@ -87,7 +87,7 @@ final class Latch {
 	 *
 	 * @param $authorizationHeader Authorization HTTP Header
 	 * @return string the requesting application Id. Identifies the application using the API
-	 */	
+	 */
 	public static function getAppIdFromHeader($authorizationHeader) {
 		return getPartFromHeader(1, $authorizationHeader);
 	}
@@ -113,44 +113,44 @@ final class Latch {
 		$this->appId = $appId;
 		$this->secretKey = $secretKey;
 	}
-	
+
 	public function HTTP_GET($url, $headers) {
 		$curlHeaders = array();
 		foreach ($headers as $hkey=>$hvalue) {
 			array_push($curlHeaders, $hkey . ":" . $hvalue);
 		}
-		
+
 		$ch = curl_init($url);
 		curl_setopt($ch, CURLOPT_HTTPHEADER, $curlHeaders);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
                 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
                 curl_setopt($ch, CURLOPT_PROXY, self::$PROXY_HOST);
-                
+
                 if (self::$CA_CERTIFICATE_PATH != NULL) {
                     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
                     curl_setopt($ch, CURLOPT_CAINFO, self::$CA_CERTIFICATE_PATH);
                 } else {
                     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
                 }
-                
+
 		$response = curl_exec($ch);
 		curl_close($ch);
-		
+
 		return $response;
 	}
-	
+
 	private function HTTP_GET_proxy($url) {
 		return new LatchResponse($this->HTTP_GET(self::$API_HOST . $url, $this->authenticationHeaders("GET", $url, null)));
 	}
-	
+
 	public function pairWithId($accountId) {
 		return $this->HTTP_GET_proxy(self::$API_PAIR_WITH_ID_URL . "/" . $accountId);
 	}
-	
+
 	public function pair($token) {
 		return $this->HTTP_GET_proxy(self::$API_PAIR_URL . "/" . $token);
 	}
-	
+
 	public function status($accountId) {
 		return $this->HTTP_GET_proxy(self::$API_CHECK_STATUS_URL . "/" . $accountId);
 	}
@@ -158,12 +158,12 @@ final class Latch {
 	public function operationStatus($accountId, $operationId) {
 		return $this->HTTP_GET_proxy(self::$API_CHECK_STATUS_URL . "/" . $accountId . "/op/" . $operationId);
 	}
-		
+
 	public function unpair($accountId) {
 		return $this->HTTP_GET_proxy(self::$API_UNPAIR_URL . "/" . $accountId);
 	}
 
-	
+
 	/**
 	 *
 	 * @param $data the string to sign
@@ -172,7 +172,7 @@ final class Latch {
 	private function signData($data) {
 		return base64_encode(hash_hmac(self::$HMAC_ALGORITHM, $data, $this->secretKey, true));
 	}
-	
+
 	/**
 	 * Calculate the authentication headers to be sent with a request to the API
 	 * @param $HTTPMethod the HTTP Method, currently only GET is supported
@@ -183,7 +183,7 @@ final class Latch {
 	 */
 	public function authenticationHeaders($HTTPMethod, $queryString, $xHeaders=null, $utc=null) {
 		$utc = trim(($utc!=null) ? $utc : $this->getCurrentUTC());
-		
+
 		//error_log($HTTPMethod);
 		//error_log($queryString);
 		//error_log($utc);
@@ -191,11 +191,11 @@ final class Latch {
 		$stringToSign = trim(strtoupper($HTTPMethod)) . "\n" .
 						$utc . "\n" .
 						$this->getSerializedHeaders($xHeaders) . "\n" .
-						trim($queryString); 					
+						trim($queryString);
 
-		$authorizationHeader = self::$AUTHORIZATION_METHOD . 
+		$authorizationHeader = self::$AUTHORIZATION_METHOD .
 							   self::$AUTHORIZATION_HEADER_FIELD_SEPARATOR .
-							   $this->appId . 
+							   $this->appId .
 							   self::$AUTHORIZATION_HEADER_FIELD_SEPARATOR .
 							   $this->signData($stringToSign);
 
@@ -207,7 +207,7 @@ final class Latch {
 
 	/**
 	 * Prepares and returns a string ready to be signed from the 11-paths specific HTTP headers received
-	 * @param $xHeaders a non necessarily ordered map of the HTTP headers to be ordered without duplicates.  
+	 * @param $xHeaders a non necessarily ordered map of the HTTP headers to be ordered without duplicates.
 	 * @return a String with the serialized headers, an empty string if no headers are passed, or null if there's a problem
 	 * such as non 11paths specific headers
 	 */
@@ -216,7 +216,7 @@ final class Latch {
 			$headers = array_change_key_case($xHeaders, CASE_LOWER);
 			ksort($headers);
 			$serializedHeaders = "";
-							
+
 			foreach($headers as $key=>$value) {
 				if(strncmp(strtolower($key), strtolower($X_11PATHS_HEADER_PREFIX), strlen($X_11PATHS_HEADER_PREFIX))==0) {
 					error_log("Error serializing headers. Only specific " . $X_11PATHS_HEADER_PREFIX . " headers need to be singed");
@@ -229,7 +229,7 @@ final class Latch {
 			return "";
 		}
 	}
-	
+
 	/**
 	 *
 	 * @return a string representation of the current time in UTC to be used in a Date HTTP Header
